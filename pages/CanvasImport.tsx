b046a2d004868cbbc505b72c5bbdb7755e5186d3
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-03-15 18:15:34 +0800

    Update CanvasImport.tsx

--- pages/CanvasImport.tsx
+++ pages/CanvasImport.tsx
@@ -1,6 +1,6 @@
 // Canvas的注意事項
 
-import { Head } from "next/document";
+import Head from "next/head";
 import Layout from "../components/layout";
 
 export function CanvasImport() {
@@ -9,14 +9,16 @@ export function CanvasImport() {
             <Head>
                 <title>Canvas的注意事項</title>
             </Head>
+
             <div>
                 <h1>這裡會給你一些Canvas的注意事項</h1>
                 <p>很多人或是我偶爾也會忘記，所以我就把它寫在這裡</p>
 
                 <p>記得要把Canvas的寬高設定好，不然你會看不到你畫的東西</p>
                 <p>記得要close path，不然你會看到很奇怪的東西</p>
-                
             </div>
         </Layout>
     )
 }
+
+export default CanvasImport;
