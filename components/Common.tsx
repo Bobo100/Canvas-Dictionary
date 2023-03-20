99ba40b6109d7c36690a4023f9006ba1eeecac82
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-03-20 17:52:29 +0800

    Update Common.tsx

--- components/Common.tsx
+++ components/Common.tsx
@@ -4,7 +4,6 @@ export const CommonPrism = ({ children }: { children: string }) => {
     return (
         <div className="code-block-container">
             <Prism className="code-block" language="typescript" style={vscDarkPlus}
-
                 showLineNumbers={true}
             >
                 {children}
